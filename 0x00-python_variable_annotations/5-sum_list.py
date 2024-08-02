670bbe005f96b3fafb321db8e44d0f781b0c0a26
ABDELRAHIM4 <abdulrhimsaeed@gmail.com>
2024-08-02 15:43:19 +0000

    python

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+from typing import List
+""" type-annotated function sum_list which takes a list input_list of floats"""
+
+
+def sum_list(input_list: List[float]) -> float:
+    """ type-annotated function sum_list which takes a list input_list"""
+    return (sum(input_list))
---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+from typing import List, Union
+"""type-annotated function sum_mixed_list which takes a list mxd_lst"""
+
+
+def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
+    """type-annotated function sum_mixed_list which takes a list mxd_lst"""
+    return (sum(mxd_lst))
