db5575b2ae253f77fdf760677bee2fbaf06a77fd
ABDELRAHIM4 <abdulrhimsaeed@gmail.com>
2024-08-05 21:53:43 +0000

    python

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -2,9 +2,9 @@
 """task_wait_random that takes an integer"""
 import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
-
-
 """task_wait_random that takes an integer"""
+
+
 def task_wait_random(max_delay: int) -> asyncio.Task:
     """task_wait_random that takes an integer"""
     task = asyncio.create_task(wait_random(max_delay))
