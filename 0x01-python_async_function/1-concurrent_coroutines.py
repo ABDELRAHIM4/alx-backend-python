e761f6fc0ddb9f22b433c6691b31ec29351534e9
ABDELRAHIM4 <abdulrhimsaeed@gmail.com>
2024-08-05 21:28:15 +0000

    python

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import asyncio
 from typing import List
+import heapq
 wait_random = __import__('0-basic_async_syntax').wait_random
 """an async routine called wait_n that takes in 2 int arguments """
 
@@ -8,5 +9,7 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """an async routine called wait_n that takes in 2 int arguments """
     tasks = [wait_random(max_delay) for _ in range(n)]
-    delay = await asyncio.gather(*tasks)
-    return (sorted(delay))
+    delay = []
+    for task in asyncio.as_completed(tasks):
+        heapq.heappush(delay, await task)
+    return (delay)
